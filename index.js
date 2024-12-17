dec044b226eb97569e4339e121f90b7721cfdc98
Ansh-699 <anshtyagi7845@gmail.com>
2024-12-17 20:24:26 +0530

    cors

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/auth'); 
-
-
-
-
-
-
+const cors = require('cors');  // Import CORS
+const authRoutes = require('./routes/auth');
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT;
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000', 
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
-    res.send('API is running...');
-  });
-
+  res.send('API is running...');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
